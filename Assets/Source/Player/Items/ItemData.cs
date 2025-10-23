cdbdbbf712820c686cf2a054e38ed4263b553e9c
Extelder <Nikolaoskas15@gmail.com>
2025-10-23 16:25:56 +0700

    add base for inventory

---
+++ Assets/Source/Player/Items/ItemData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory/ItemData", fileName = "ItemData")]
+public class ItemData : ScriptableObject
+{
+    [field: SerializeField] public int Id { get; private set; }
+    [field: SerializeField] public string Name { get; private set; }
+}
