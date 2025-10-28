96fbc381058c3a860ab22544fa4c575b4237f0cc
zekelty <108453722+zekelty@users.noreply.github.com>
2025-10-28 19:52:24 +0700

    add room

---
+++ Assets/Source/Trap/SpawnableTrap/SpawnableTrap.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnableTrap : PlayerTrigger
+{
+    [SerializeField] private GameObject _objectOriginal;
+    [SerializeField] private GameObject _objectTrap;
+    [SerializeField] private float _delay;
+
+    public override void OnTriggered(PlayerHealth playerHealth)
+    {
+        if(Random.Range(1,100) >= 90) StartCoroutine(Triggering());
+    }
+
+    private IEnumerator Triggering()
+    {
+         yield return new WaitForSeconds(_delay);
+         _objectOriginal.SetActive(false);
+         _objectTrap.SetActive(true);
+    }
+}
