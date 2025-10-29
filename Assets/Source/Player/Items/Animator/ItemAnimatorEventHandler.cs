2fdc2efab882367e65ab08b99902bd6bee5aac3a
igoryava <126491702+igoryava@users.noreply.github.com>
2025-10-29 18:03:45 +0700

    fixed ya dolboeb

--- Assets/Source/Player/Items/Animator/ItemAnimatorEventHandler.cs
+++ Assets/Source/Player/Items/Animator/ItemAnimatorEventHandler.cs
@@ -12,7 +12,6 @@ public class ItemAnimatorEventHandler : MonoBehaviour
     {
         AnimationEndWithoutChecking();
         _takeUp.TakeUp();
-        _currentItemAnimator?.AnimationReset();
         _currentItemAnimator = itemAnimator;
     }
 
