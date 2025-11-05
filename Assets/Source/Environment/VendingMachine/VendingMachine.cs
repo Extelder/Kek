3cbade442b020541c1c5e4da0469cbb780d82269
igoryava <126491702+igoryava@users.noreply.github.com>
2025-11-05 22:42:53 +0700

    added animations for vending

--- Assets/Source/Environment/VendingMachine/VendingMachine.cs
+++ Assets/Source/Environment/VendingMachine/VendingMachine.cs
@@ -1,14 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 public class VendingMachine : MonoBehaviour
 {
     [SerializeField] private Transform _spawnOrigin;
+    [SerializeField] private NetWorkAnimatorSynchronize _animatorSynchronize;
+    [SerializeField] private string _boolName;
+    [SerializeField] private float _coolDown;
 
-    public void Spawn(BuyableItemData buyableItemData)
+    [field: SerializeField] public bool CanInteract { get; private set; } = true;
+    private BuyableItemData _currentBuyable;
+    
+    public void GetCurrentBuyable(BuyableItemData buyableItemData)
     {
-        PlayerCharacter.Instance.ServerSpawnObject(buyableItemData.Prefab, _spawnOrigin.position, Quaternion.identity);
+        Debug.Log("OnBought");
+        _currentBuyable = buyableItemData;
+        _animatorSynchronize.SetAnimatorBoolMulticast(_boolName, true);
+        CanInteract = false;
+    }
+    
+    public void SpawnItem()
+    {
+        PlayerCharacter.Instance.ServerSpawnObject(_currentBuyable.Prefab, _spawnOrigin.position, Quaternion.identity);
+        StartCoroutine(DisableBool());
+    }
+
+    private IEnumerator DisableBool()
+    {
+        yield return new WaitForSeconds(_coolDown);
+        _animatorSynchronize.SetAnimatorBoolMulticast(_boolName, false);
+        CanInteract = true;
     }
 }
--- Assets/Source/Item/Interact/VendingMachine/VendingMachineInteractableSpawnable.cs
+++ Assets/Source/Item/Interact/VendingMachine/VendingMachineInteractableSpawnable.cs
@@ -1,25 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class VendingMachineInteractableSpawnable : VendingMachineInteractible
 {
-    [SerializeField] private VendingMachine _vendingMachine;
-
-    public event Action<string> TextChanged;
-    public event Action<int> PriceChanged;
-
+    public event Action<string, int, Sprite> DataChanged;
+    
     public override void OnStartClient()
     {
         base.OnStartClient();
-        TextChanged?.Invoke(ItemData.Name);
-        PriceChanged?.Invoke(ItemData.Price);
+        DataChanged?.Invoke(ItemData.Name, ItemData.Price, ItemData.Image);
     }
 
     public override void OnBought()
     {
-        _vendingMachine.Spawn(ItemData);
+        VendingMachine.GetCurrentBuyable(ItemData);
     }
 }
