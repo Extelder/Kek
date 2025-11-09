25a89dd6c5560e42b1b68748ddb3fe2bb204f371
igoryava <126491702+igoryava@users.noreply.github.com>
2025-11-09 19:10:53 +0700

    fixed kait

--- Assets/Source/Environment/Collect/CollectTrigger.cs
+++ Assets/Source/Environment/Collect/CollectTrigger.cs
@@ -14,7 +14,7 @@ public class CollectTrigger : NetworkBehaviour
         {
             if (interactItem.Item is PickUpableItem pickUpableItem)
             {
-                PlayerCharacter.Instance.Wallet.Add(pickUpableItem.Price);
+                PlayerCharacter.Instance.PlayerWallet.Add(pickUpableItem.Price);
                 
                 interactItem.Despawn();
             }
