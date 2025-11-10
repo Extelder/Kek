222fe7a5b74097862dedbb96f1de9a9c8af845e2
igoryava <126491702+igoryava@users.noreply.github.com>
2025-11-10 19:31:39 +0700

    added slope movement

--- Assets/Source/Player/Movement/PlayerMovement.cs
+++ Assets/Source/Player/Movement/PlayerMovement.cs
@@ -8,11 +8,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : NetworkBehaviour
 {
+    [field :SerializeField] public float Speed { get; private set; }
+    public Vector3 InputVector { get; private set; } = new Vector3(0, 0, 0);
+    
     [SerializeField] private PlayerAnimator _animator;
 
     [SerializeField] private PlayerCharacter _character;
 
-    [SerializeField] private float _speed;
     [SerializeField] private float _runSpeedMultiplier;
 
     [SerializeField] private GroundChecker _groundChecker;
@@ -23,8 +25,7 @@ public class PlayerMovement : NetworkBehaviour
     private PlayerBinds _binds;
 
     private CompositeDisposable _disposable = new CompositeDisposable();
-
-
+    
     private Vector3 _currentVelocity;
 
     public bool CanFly;
@@ -40,11 +41,9 @@ public class PlayerMovement : NetworkBehaviour
         _rigidbody = _character.Rigidbody;
         _binds = _character.Binds;
 
-        Vector3 inputVector = new Vector3(0, 0, 0);
-
         Observable.EveryUpdate().Subscribe(_ =>
         {
-            inputVector = new Vector3(_binds.Character.Horizontal.ReadValue<float>(), 0,
+            InputVector = new Vector3(_binds.Character.Horizontal.ReadValue<float>(), 0,
                 _binds.Character.Vertical.ReadValue<float>());
         }).AddTo(_disposable);
 
@@ -55,14 +54,14 @@ public class PlayerMovement : NetworkBehaviour
         {
             if (!IsOwner)
                 return;
-            inputVector = transform.TransformDirection(inputVector);
+            InputVector = transform.TransformDirection(InputVector);
 
-            inputVector.Normalize();
+            InputVector.Normalize();
 
-            Moving.Value = Mathf.Abs(inputVector.x) > 0 || Mathf.Abs(inputVector.z) > 0;
+            Moving.Value = Mathf.Abs(InputVector.x) > 0 || Mathf.Abs(InputVector.z) > 0;
 
-            Vector3 desiredVelocityXZ = new Vector3(inputVector.x * _speed, 0,
-                inputVector.z * _speed);
+            Vector3 desiredVelocityXZ = new Vector3(InputVector.x * Speed, 0,
+                InputVector.z * Speed);
 
             if (Moving.Value || _groundChecker.Detected)
                 _currentVelocity =
@@ -77,7 +76,7 @@ public class PlayerMovement : NetworkBehaviour
             {
                 float fly = _binds.Character.FlyUpDown.ReadValue<float>();
                 _rigidbody.velocity =
-                    new Vector3(_currentVelocity.x, fly * _speed, _currentVelocity.z);
+                    new Vector3(_currentVelocity.x, fly * Speed, _currentVelocity.z);
                 return;
             }
 
@@ -88,13 +87,13 @@ public class PlayerMovement : NetworkBehaviour
 
     private void OnRunCanceled(InputAction.CallbackContext obj)
     {
-        _speed /= _runSpeedMultiplier;
+        Speed /= _runSpeedMultiplier;
         _animator.SetLocomotionBlendTreeSpeed(1f);
     }
 
     private void OnRunStarted(InputAction.CallbackContext obj)
     {
-        _speed *= _runSpeedMultiplier;
+        Speed *= _runSpeedMultiplier;
         _animator.SetLocomotionBlendTreeSpeed(1.5f);
     }
 
--- Assets/Source/Player/Movement/Slope/PlayerSlopeMovement.cs
+++ Assets/Source/Player/Movement/Slope/PlayerSlopeMovement.cs
@@ -12,7 +12,13 @@ public class PlayerSlopeMovement : NetworkBehaviour
     [SerializeField] private RaycastSettings _raycastSettings;
 
     [SerializeField] private PlayerMovement _playerMovement;
+    
     [SerializeField] private float _checkRate;
+    [SerializeField] private float _maxSlopeAngle;
+
+    private RaycastHit _hit;
+
+    private PlayerCharacter _character;
     
     private CompositeDisposable _reactiveDisposable = new CompositeDisposable();
     private CompositeDisposable _checkDisposable = new CompositeDisposable();
@@ -22,10 +28,15 @@ public class PlayerSlopeMovement : NetworkBehaviour
         base.OnStartClient();
         if (!base.IsOwner)
             return;
+        _character = PlayerCharacter.Instance;
         _playerMovement.Moving.Subscribe(_ =>
         {
             if (_)
+            {
                 CheckNormalAngle();
+                return;
+            }
+            _checkDisposable.Clear();
         }).AddTo(_reactiveDisposable);
     }
 
@@ -33,15 +44,36 @@ public class PlayerSlopeMovement : NetworkBehaviour
     {
         Observable.Interval(TimeSpan.FromSeconds(_checkRate)).Subscribe(_ =>
         {
-            bool hitted = Physics.Raycast(_raycastSettings.Origin.position, Vector3.down, out RaycastHit hit,
-                _raycastSettings.MaxDistance, _raycastSettings.LayerMask);
-            if (hitted)
+            if (OnSlope())
             {
-                if (hit.collider.TryGetComponent<Ground>(out Ground ground))
-                {
-                }
+                _character.Rigidbody.AddForce(GetSlopeMoveDirection() * _playerMovement.Speed * 20f, ForceMode.Force);
+
+                if (_character.Rigidbody.velocity.y > 0)
+                    _character.Rigidbody.AddForce(Vector3.down * 80f, ForceMode.Force);
             }
         }).AddTo(_checkDisposable);
+    }
+    
+    private bool OnSlope()
+    {
+        bool hitted = Physics.Raycast(_raycastSettings.Origin.position, Vector3.down, out _hit,
+            _raycastSettings.MaxDistance, _raycastSettings.LayerMask);
+        if (hitted)
+        {
+            if (_hit.collider.TryGetComponent<Ground>(out Ground ground))
+            {
+                Debug.Log("GroundChecked");
+                float angle = Vector3.Angle(Vector3.up, _hit.normal);
+                return angle < _maxSlopeAngle && angle != 0;
+            }
+        }
+
+        return false;
+    }
+    
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(_playerMovement.InputVector, _hit.normal).normalized;
     }
 
     private void OnDisable()
