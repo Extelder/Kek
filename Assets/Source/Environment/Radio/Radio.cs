efcb4b5fab2774464aa8a1cecf1f4c59cf7d8051
Extelder <Nikolaoskas15@gmail.com>
2025-11-10 19:22:35 +0700

    compleate smoke tnt throw

---
+++ Assets/Source/Environment/Radio/Radio.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using FishNet.Object;
+using UnityEngine;
+
+public class Radio : NetworkBehaviour, IInteractable
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    public void Interact()
+    {
+        SetSoundPause(!_audioSource.isPlaying);
+    }
+
+    [ServerRpc]
+    public void SetSoundPause(bool pause)
+    {
+        SetSoundPauseObserver(pause);
+    }
+
+    [ObserversRpc]
+    public void SetSoundPauseObserver(bool pause)
+    {
+        if (pause)
+            _audioSource.Pause();
+        else
+            _audioSource.UnPause();
+    }
+}
--- Assets/Source/Player/Items/Animator/TNTAnimator.cs
+++ Assets/Source/Player/Items/Animator/TNTAnimator.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class TNTAnimator : ItemAnimator
 {
+    [SerializeField] private PlayerInventory _inventory;
     [SerializeField] private GameObject _tntThrowablePrefab;
     [SerializeField] private AudioSource _sound;
-    
+
     public override void Attack()
     {
         PlayerCharacter.Instance.ServerSpawnObject(_tntThrowablePrefab, PlayerCharacter.Instance.DropPoint.position,
             PlayerCharacter.Instance.CameraTransform.rotation);
+        _inventory.ClearEquipSlot();
         _sound.Play();
     }
-    
+
     public override void AnimationEndCheck()
     {
         Animator.ThrowAnim();
