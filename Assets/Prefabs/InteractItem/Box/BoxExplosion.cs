ff50ddb40702649b4b798e67829c2f0038353377
zekelty <108453722+zekelty@users.noreply.github.com>
2025-11-16 17:23:47 +0700

    add landerdestroyable

--- Assets/Prefabs/InteractItem/Box/BoxExplosion.cs
+++ Assets/Prefabs/InteractItem/Box/BoxExplosion.cs
@@ -6,32 +6,70 @@ using UnityEngine;
 public class BoxExplosion : NetworkBehaviour
 {
     [SerializeField] private GameObject[] _object;
+    [SerializeField] private GameObject[] _objectDestroyAfterMain;
     [SerializeField] private float _strenge;
+    [SerializeField] private bool destroyAll;
     private bool IsUse;
-    
+    private int _currentIndex;
+
     [ServerRpc(RequireOwnership = false)]
     public void Interact()
     {
         if (!IsUse)
         {
-            IsUse = true;
             InteractObserver();
         }
     }
+
     [ObserversRpc]
     private void InteractObserver()
     {
+        if (!destroyAll)
+        {
+            Explosion(_currentIndex);
+            _currentIndex++;
+            if (_object.Length == _currentIndex)
+            {
+                GetComponent<Collider>().enabled = false;
+                IsUse = true;
+                if (_objectDestroyAfterMain.Length > 0)
+                {
+                    for (int i = 0; i < _objectDestroyAfterMain.Length; i++)
+                    {
+                        ExplosionAfter(i);
+                    }
+                }
+            }
+
+            return;
+        }
+
+        IsUse = true;
         GetComponent<Collider>().enabled = false;
         for (int i = 0; i < _object.Length; i++)
         {
-            Rigidbody rb = _object[i].GetComponent<Rigidbody>();
-            rb.isKinematic = false;
-            Vector3 dir = (Vector3.up * 0.3f) + (Random.insideUnitSphere * 0.2f);
-            dir.Normalize();
-            float force = _strenge;
-            rb.AddForce(dir * force, ForceMode.VelocityChange);
-            rb.AddTorque(Random.insideUnitSphere * 5f, ForceMode.VelocityChange);
+            Explosion(i);
         }
     }
-    
+
+    private void Explosion(int index)
+    {
+        Rigidbody rb = _object[index].GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        Vector3 dir = (Vector3.up * 0.3f) + (Random.insideUnitSphere * 0.2f);
+        dir.Normalize();
+        float force = _strenge;
+        rb.AddForce(dir * force, ForceMode.VelocityChange);
+        rb.AddTorque(Random.insideUnitSphere * 5f, ForceMode.VelocityChange);
+    }
+    private void ExplosionAfter(int index)
+    {
+        Rigidbody rb = _objectDestroyAfterMain[index].GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        Vector3 dir = (Vector3.up * 0.3f) + (Random.insideUnitSphere * 0.2f);
+        dir.Normalize();
+        float force = _strenge;
+        rb.AddForce(dir * force, ForceMode.VelocityChange);
+        rb.AddTorque(Random.insideUnitSphere * 5f, ForceMode.VelocityChange);
+    }
 }
