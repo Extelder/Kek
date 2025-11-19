31f4f509937113f6fc44111d585b04c255bbe410
zekelty <108453722+zekelty@users.noreply.github.com>
2025-11-19 23:54:45 +0700

    add FlyPivo

--- Assets/Prefabs/InteractItem/Box/BoxExplosion.cs
+++ Assets/Prefabs/InteractItem/Box/BoxExplosion.cs
@@ -9,6 +9,7 @@ public class BoxExplosion : NetworkBehaviour
     [SerializeField] private GameObject[] _objectDestroyAfterMain;
     [SerializeField] private float _strenge;
     [SerializeField] private bool destroyAll;
+    [SerializeField] private bool _lockInteractAfterUse = true;
     private bool IsUse;
     private int _currentIndex;
 
@@ -27,11 +28,18 @@ public class BoxExplosion : NetworkBehaviour
         if (!destroyAll)
         {
             Explosion(_currentIndex);
-            _currentIndex++;
+            if (_lockInteractAfterUse)
+            {
+                _currentIndex++;
+            }
             if (_object.Length == _currentIndex)
             {
-                GetComponent<Collider>().enabled = false;
-                IsUse = true;
+                if (_lockInteractAfterUse)
+                {
+                    IsUse = true;
+                    GetComponent<Collider>().enabled = false;
+                }
+
                 if (_objectDestroyAfterMain.Length > 0)
                 {
                     for (int i = 0; i < _objectDestroyAfterMain.Length; i++)
@@ -62,6 +70,7 @@ public class BoxExplosion : NetworkBehaviour
         rb.AddForce(dir * force, ForceMode.VelocityChange);
         rb.AddTorque(Random.insideUnitSphere * 5f, ForceMode.VelocityChange);
     }
+
     private void ExplosionAfter(int index)
     {
         Rigidbody rb = _objectDestroyAfterMain[index].GetComponent<Rigidbody>();
