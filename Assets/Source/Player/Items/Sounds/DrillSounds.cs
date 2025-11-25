6e0d5a9f5000dc0623a29ec7c64b0d8c008e812f
igoryava <126491702+igoryava@users.noreply.github.com>
2025-11-25 21:29:03 +0700

    added hit sounds for enemies and ore and some shit

--- Assets/Source/Player/Items/Sounds/DrillSounds.cs
+++ Assets/Source/Player/Items/Sounds/DrillSounds.cs
@@ -5,52 +5,28 @@ using UnityEngine;
 
 public class DrillSounds : MonoBehaviour
 {
-    [SerializeField] private MineableItemAnimator _animator;
     [SerializeField] private Drill _drill;
     [SerializeField] private SoundPlayPause _soundPlayPause;
-    [SerializeField] private SoundPlayPause _secondSoundPlayPause;
     
     private void OnEnable()
     {
         _drill.StartedDrilling += OnStartedDrilling;
         _drill.StoppedDrilling += OnStoppedDrilling;
-        _animator.Hitted += OnHitted;
-        _animator.NotHitted += OnNotHitted;
     }
 
     private void OnStoppedDrilling()
     {
-        _secondSoundPlayPause.Pause(true);
         _soundPlayPause.Pause(true);
     }
 
     private void OnStartedDrilling(bool value)
     {
-        _secondSoundPlayPause.Pause(value);
-        _soundPlayPause.Pause(true);
-    }
-
-    private void OnHitted()
-    {
-        _soundPlayPause.Pause(false);
-    }
-    
-    private void OnNotHitted()
-    {
-        StopSound();
-    }
-
-    private void StopSound()
-    {
-        _soundPlayPause.Pause(true);
+        _soundPlayPause.Pause(value);
     }
 
     private void OnDisable()
     {
         _drill.StartedDrilling -= OnStartedDrilling;
         _drill.StoppedDrilling -= OnStoppedDrilling;
-        _animator.Hitted -= OnHitted;
-        _animator.NotHitted -= OnNotHitted;
-        StopSound();
     }
 }
