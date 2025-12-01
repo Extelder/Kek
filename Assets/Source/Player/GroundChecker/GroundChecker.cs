3d58aed0bf2afd8db70c7d5e69b77519ecaac46c
zekelty <108453722+zekelty@users.noreply.github.com>
2025-12-01 15:41:45 +0700

    add footsteps on Iron

--- Assets/Source/Player/GroundChecker/GroundChecker.cs
+++ Assets/Source/Player/GroundChecker/GroundChecker.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum FootStep
+{
+    rock, iron
+}
 public class GroundChecker : MonoBehaviour
 {
+    public FootStep Foot;
     [SerializeField] private RaycastSettings _raycastSettings;
     public bool Detected { get; private set; }
-
     private void FixedUpdate()
     {
         if (Physics.Raycast(_raycastSettings.Origin.position, -_raycastSettings.Origin.up, out RaycastHit hit,
@@ -15,10 +18,19 @@ public class GroundChecker : MonoBehaviour
             _raycastSettings.LayerMask))
         {
             Detected = true;
+            if (hit.collider.TryGetComponent(out BoerCheck boerCheck))
+            {
+                Foot = FootStep.iron;
+            }
+            else
+            {
+                Foot = FootStep.rock;
+            }
         }
         else
         {
             Detected = false;
+            
         }
     }
 
--- Assets/Source/Player/Sounds/EnemyFootSteps.cs
+++ Assets/Source/Player/Sounds/EnemyFootSteps.cs
@@ -3,40 +3,61 @@ using System.Collections.Generic;
 using FishNet.Object;
 using UnityEngine;
 
-public class EnemyFootSteps : NetworkBehaviour
+public class EnemyFootSteps : MonoBehaviour
 {
     [SerializeField] private AudioSource _audio;
-    [SerializeField] private AudioClip[] _audioClip;
+    [SerializeField] private AudioClip[] _audioClipRock;
+    [SerializeField] private AudioClip[] _audioClipIron;
+    [SerializeField] private GroundChecker _ground;
+    [SerializeField] private float _VolumeIron;
+    [SerializeField] private float _VolumeRock;
     private AudioClip _oldclip;
     private int _lastIndex;
-    private int _lastTwoIndex;
-    [ServerRpc(RequireOwnership = false)]
+
     public void RandomizeFootSteps()
     {
-        RandomizeObserver();
+        RandomizeObserver(_ground.Foot);
     }
 
-    public int GetRandomIndex()
+    public int GetRandomIndexRock()
     {
-        int index = Random.Range(0, _audioClip.Length - 1);
+        int index = Random.Range(0, _audioClipRock.Length - 1);
         if (index == _lastIndex)
         {
-            return GetRandomIndex();
-        }
-        if (index == _lastTwoIndex)
-        {
-            return GetRandomIndex();
+            return GetRandomIndexRock();
         }
 
-        _lastTwoIndex = _lastIndex;
         return index;
     }
 
-    [ObserversRpc]
-    private void RandomizeObserver()
+    public int GetRandomIndexIron()
     {
-        _lastIndex = GetRandomIndex();
-        _audio.clip = _audioClip[_lastIndex];
-        _audio.Play();
+        int index = Random.Range(0, _audioClipRock.Length - 1);
+        if (index == _lastIndex)
+        {
+            return GetRandomIndexIron();
+        }
+
+        return index;
+    }
+
+    private void RandomizeObserver(FootStep foot)
+    {
+        if (foot == FootStep.iron)
+        {
+            _lastIndex = GetRandomIndexIron();
+            _audio.clip = _audioClipIron[_lastIndex];
+            _audio.volume = _VolumeIron;
+            _audio.Play();
+            return;
+        }
+
+        if (foot == FootStep.rock)
+        {
+            _lastIndex = GetRandomIndexRock();
+            _audio.clip = _audioClipRock[_lastIndex];
+            _audio.volume = _VolumeRock;
+            _audio.Play();
+        }
     }
 }
